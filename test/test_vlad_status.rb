36bbd18ca87e67311ea8a09cfce57ca3957411ce
Joshua Mervine <joshua@mervine.net>
2011-07-22 16:57:20 -0700

    removing everything from this branch

--- test/test_vlad_status.rb
+++
@@ -1,9 +0,0 @@
-require "test/unit"
-require "vlad/status"
-
-class TestVladPush < Test::Unit::TestCase
-  def test_sanity
-    assert system("rake -T vlad")
-    assert system("rake -T vald | grep status")
-  end
-end
